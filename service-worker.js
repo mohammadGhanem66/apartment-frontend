9efe3c22efbd58cc01a7c9b3af3fcd0139f7edd3
Safaa Duridi <mghanem@360moms.net>
2024-12-29 04:18:48 +0200

    service worker fixes

--- service-worker.js
+++ service-worker.js
@@ -13,106 +13,93 @@ const assetsToCache = [
   'lib/jquery/jquery.min.js'
 ];
 
-// self.addEventListener('install', (event) => {
-//   console.log("service worker start installing !!");
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(cache => {
-//       cache.addAll(assetsToCache);
-//     })
-//   );
-//   // event.waitUntil(
-//   //   caches.open(CACHE_NAME)
-//   //     .then((cache) => {
-//   //       return cache.addAll(assetsToCache);
-//   //     })
-//   // );
-// });
 self.addEventListener('install', (event) => {
-  self.skipWaiting(); // Activate the new service worker immediately
-});
-// self.addEventListener('activate', (event) => {
-//   console.log("Service worker activated");
-//   event.waitUntil(
-//     caches.keys().then(keys => {
-//       console.log(keys);
-//       return Promise.all(
-//         keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-//       )
-//     })
-//   );
-
-// });
-self.addEventListener('activate', (event) => {
+  console.log("service worker start installing !!");
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME && key !== dynamicCahceName)
-            .map((key) => caches.delete(key))
-      );
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assetsToCache);
     })
   );
-  return self.clients.claim(); // Claim control of all active tabs
+  // event.waitUntil(
+  //   caches.open(CACHE_NAME)
+  //     .then((cache) => {
+  //       return cache.addAll(assetsToCache);
+  //     })
+  // );
+});
+self.addEventListener('activate', (event) => {
+  console.log("Service worker activated");
+  event.waitUntil(
+    caches.keys().then(keys => {
+      console.log(keys);
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    })
+  );
+
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log(event.request);
+
+  event.respondWith(
+    caches.match(event.request).then(cacheRes => {
+      return cacheRes || fetch(event.request).then(fetchRes => {
+        return caches.open(dynamicCahceName).then(cache => {
+          cache.put(event.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      })
+    }).catch(() => {
+      if (event.request.headers.get('accept').includes('text/html')) {
+        return caches.match('/');
+      }
+    })
+  );
+  
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response) => {
+//         return response || fetch(event.request);
+//       })
+//   );
 });
 // self.addEventListener('fetch', (event) => {
-//   console.log(event.request);
+//   const requestUrl = new URL(event.request.url);
 
-//   event.respondWith(
-//     caches.match(event.request).then(cacheRes => {
-//       return cacheRes || fetch(event.request).then(fetchRes => {
-//         return caches.open(dynamicCahceName).then(cache => {
-//           cache.put(event.request.url, fetchRes.clone());
-//           return fetchRes;
+//   // Check if the request is for dynamic content (e.g., API)
+//   if (requestUrl.pathname.startsWith('/api')) {
+//     // Network-first strategy for API requests
+//     event.respondWith(
+//       fetch(event.request)
+//         .then(fetchRes => {
+//           // Update the dynamic cache with the new response
+//           return caches.open(dynamicCahceName).then(cache => {
+//             cache.put(event.request, fetchRes.clone());
+//             return fetchRes;
+//           });
 //         })
+//         .catch(() => {
+//           // Fall back to cache if network fails
+//           return caches.match(event.request);
+//         })
+//     );
+//   } else {
+//     // Cache-first strategy for other requests
+//     event.respondWith(
+//       caches.match(event.request).then(cacheRes => {
+//         return cacheRes || fetch(event.request).then(fetchRes => {
+//           return caches.open(dynamicCahceName).then(cache => {
+//             cache.put(event.request, fetchRes.clone());
+//             return fetchRes;
+//           });
+//         });
+//       }).catch(() => {
+//         if (event.request.headers.get('accept').includes('text/html')) {
+//           return caches.match('/');
+//         }
 //       })
-//     }).catch(() => {
-//       if (event.request.headers.get('accept').includes('text/html')) {
-//         return caches.match('/');
-//       }
-//     })
-//   );
-  
-// //   event.respondWith(
-// //     caches.match(event.request)
-// //       .then((response) => {
-// //         return response || fetch(event.request);
-// //       })
-// //   );
+//     );
+//   }
 // });
-self.addEventListener('fetch', (event) => {
-  const requestUrl = new URL(event.request.url);
-
-  // Check if the request is for dynamic content (e.g., API)
-  if (requestUrl.pathname.startsWith('/api')) {
-    // Network-first strategy for API requests
-    event.respondWith(
-      fetch(event.request)
-        .then(fetchRes => {
-          // Update the dynamic cache with the new response
-          return caches.open(dynamicCahceName).then(cache => {
-            cache.put(event.request, fetchRes.clone());
-            return fetchRes;
-          });
-        })
-        .catch(() => {
-          // Fall back to cache if network fails
-          return caches.match(event.request);
-        })
-    );
-  } else {
-    // Cache-first strategy for other requests
-    event.respondWith(
-      caches.match(event.request).then(cacheRes => {
-        return cacheRes || fetch(event.request).then(fetchRes => {
-          return caches.open(dynamicCahceName).then(cache => {
-            cache.put(event.request, fetchRes.clone());
-            return fetchRes;
-          });
-        });
-      }).catch(() => {
-        if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('/');
-        }
-      })
-    );
-  }
-});
